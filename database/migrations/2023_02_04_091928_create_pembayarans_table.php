c8ae789357f5ff772346624edb01855d65d7bec8
thessa01 <thessaoktaviana818@gmail.com>
2023-02-04 16:28:26 +0700

    membuat tabel pendaftaran dan pembayaran

---
+++ database/migrations/2023_02_04_091928_create_pembayarans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pembayarans', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_pembayaran')->unique();
+            $table->integer('id_pendaftaran');
+            $table->integer('id_pihakasrama');
+            $table->date('tgl_masuk');
+            $table->integer('biaya');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pembayarans');
+    }
+};
---
+++ database/migrations/2023_02_04_091946_create_pendaftarans_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pendaftarans', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_pendaftaran')->unique();
+            $table->integer('KTP');
+            $table->string('nama');
+            $table->string('alamat');
+            $table->integer('nomor_hp');
+            $table->integer('umur');
+            $table->string('jenjang');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pendaftarans');
+    }
+};
